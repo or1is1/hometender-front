95f2a541953967d7ba6f83970d5ccf9c16d7467b
or1is1 <0x7for1is1@gmail.com>
2024-03-05 20:05:32 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/or1is1/hometender/front/HometenderFrontApplication.java
@@ -0,0 +1,13 @@
+package com.or1is1.hometender.front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HometenderFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HometenderFrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/or1is1/hometender/front/HometenderFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.or1is1.hometender.front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HometenderFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
